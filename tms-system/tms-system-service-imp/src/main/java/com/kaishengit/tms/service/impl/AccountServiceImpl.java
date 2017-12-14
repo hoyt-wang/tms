80ddc59e12b56b4b5adedf42a23cce680fb15c4f
hoyt-wang <hoyt_wang@yahoo.com>
2017-12-14 09:22:55 +0800

    系统管理登录&权限验证

---
+++ tms-system/tms-system-service-imp/src/main/java/com/kaishengit/tms/service/impl/AccountServiceImpl.java
@@ -0,0 +1,81 @@
+package com.kaishengit.tms.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.kaishengit.tms.entity.Account;
+import com.kaishengit.tms.entity.AccountExample;
+import com.kaishengit.tms.entity.AccountLoginLog;
+import com.kaishengit.tms.entity.Role;
+import com.kaishengit.tms.mapper.AccountLoginLogMapper;
+import com.kaishengit.tms.mapper.AccountMapper;
+import com.kaishengit.tms.mapper.AccountRoleMapper;
+import com.kaishengit.tms.service.AccountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by hoyt on 2017/12/13.
+ */
+
+@Service(version = "1.0",timeout = 5000)
+public class AccountServiceImpl implements AccountService{
+
+    private static Logger logger = LoggerFactory.getLogger(AccountServiceImpl.class);
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    @Autowired
+    private AccountLoginLogMapper accountLoginLogMapper;
+
+    @Autowired
+    private AccountRoleMapper accountRoleMapper;
+
+
+    /**
+     * 根据Account的ID查询所拥有的角色列表
+     * @param id
+     * @return
+     */
+    @Override
+    public List<Role> findRoleByAccountId(Integer id) {
+        return accountRoleMapper.findByAccountId(id);
+    }
+
+    /**
+     * 根据手机号码查询Account对象
+     * @param mobile
+     * @return
+     */
+    @Override
+    public Account findAccountByMobile(String mobile) {
+
+        AccountExample accountExample = new AccountExample();
+        accountExample.createCriteria().andAccountMobileEqualTo(mobile);
+
+        List<Account> accountList = accountMapper.selectByExample(accountExample);
+        if(accountList != null && !accountList.isEmpty()) {
+            return accountList.get(0);
+        }
+        return null;
+    }
+
+    /**
+     * 记录用户登录的日志
+     * @param account
+     * @param ip
+     */
+    @Override
+    public void saveLoginLog(Account account, String ip) {
+
+        AccountLoginLog loginLog = new AccountLoginLog();
+        loginLog.setAccountId(account.getId());
+        loginLog.setLoginIp(ip);
+        loginLog.setLoginTime(new Date());
+        accountLoginLogMapper.insertSelective(loginLog);
+        logger.info("accountLogin: {} - {} - {}",account.getId(),new Date(),ip);
+    }
+}
---
+++ tms-system/tms-system-service/src/main/java/com/kaishengit/tms/service/AccountService.java
@@ -0,0 +1,34 @@
+package com.kaishengit.tms.service;
+
+import com.kaishengit.tms.entity.Account;
+import com.kaishengit.tms.entity.Role;
+
+import java.util.List;
+
+/**
+ * TMS账号业务层
+ * @author fankay
+ */
+public interface AccountService {
+
+    /**
+     * 根据Account的ID查询所拥有的角色列表
+     * @param id
+     * @return
+     */
+    List<Role> findRoleByAccountId(Integer id);
+
+    /**
+     * 根据手机号码查询Account对象
+     * @param mobile
+     * @return
+     */
+    Account findAccountByMobile(String mobile);
+
+    /**
+     * 记录用户登录的日志
+     * @param account
+     * @param ip
+     */
+    void saveLoginLog(Account account, String ip);
+}
